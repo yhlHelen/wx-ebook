aef65acf3617b97cd877c0b1bdc1e5946326e02a
yanhailun <931511639@qq.com>
2020-04-16 17:01:16 +0800

    second-commit

---
+++ src/components/City.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="city">
+    <span  @click="handleChange()">{{this.test}}</span>
+  </div>
+</template>
+
+<script>
+    import { mapGetters } from 'vuex'
+  export default {
+      name: 'City',
+      methods: {
+          handleChange() {
+              this.$store.dispatch('setTest', 10)
+          },
+          showData() {
+              console.log(mapGetters)
+          }
+      },
+      mounted() {
+        this.showData()
+      },
+      computed: {
+          ...mapGetters(['test'])
+      }
+  }
+</script>
+
+<style scoped>
+
+</style>
